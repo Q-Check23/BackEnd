473cd4e07f84f5e193d681bb5622c4d8a7ede6cf
jyun27 <kimjyun27@gmail.com>
2026-01-04 21:43:31 +0900

    first commit

---
+++ src/main/java/team23/q_check/QCheckApplication.java
@@ -0,0 +1,13 @@
+package team23.q_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team23/q_check/QCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package team23.q_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
